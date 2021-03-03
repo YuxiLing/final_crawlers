d9d16c398de6b24b82f6de544b9aadb8c7c615a2
yuxiling <lingyuxi057@gmail.com>
2021-03-03 23:20:29 +0800

    refresh crawler counter

--- crawlers/chrome_crawler.py
+++ crawlers/chrome_crawler.py
@@ -89,7 +89,7 @@ def crawler_start(completed_count):
     
 
 if __name__=='__main__':
-    completed_count=5
+    completed_count=14
     log_file='log/chrome_log.txt'
     interval=3600 # one hour
     while True:
